4eefe9a51fb3a1423086c9f9ec42ac5410d645dc
Javier de Santiago <j.desantiagosoto@gmail.com>
2025-02-13 11:58:48 +0100

    feat: add route definitions and new route components
    Introduce route configurations for "about," "projects," and "cv" sections with nested routes. Create and link new components for "about-home," "projects-home," "cv-personal," and "cv-upm".

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
-import type {ReactNode} from "react";
+import type { ReactNode } from "react";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -63,11 +63,11 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
+    <main className="container mx-auto p-4 pt-16">
       <h1>{message}</h1>
       <p>{details}</p>
       {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
+        <pre className="w-full overflow-x-auto p-4">
           <code>{stack}</code>
         </pre>
       )}
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,16 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("./routes/home.tsx"),
+
+  route("about", "./routes/about.tsx", [index("./routes/about-home.tsx")]),
+
+  route("projects", "./routes/projects.tsx", [
+    index("./routes/projects-home.tsx"),
+  ]),
+
+  route("cv", "./routes/cv.tsx", [
+    index("./routes/cv-personal.tsx"),
+    route("upm", "./routes/cv-upm.tsx"),
+  ]),
+] satisfies RouteConfig;
