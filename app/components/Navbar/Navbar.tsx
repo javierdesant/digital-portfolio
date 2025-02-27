3c24635073ba3dffd682f33a06140b76519017c3
Javier de Santiago <j.desantiagosoto@gmail.com>
2025-02-27 11:10:19 +0100

    refactor: extract Navbar icons to improve legibility.
    Replaced inline SVGs for the burger and dropdown icons with reusable `BurgerIcon` and `DropdownIcon` components.

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import routes from "~/routes";
 import { capitalizeFirstLetter } from "~/utils";
+import DropdownIcon from "~/components/Navbar/DropdownIcon";
+import BurgerIcon from "~/components/Navbar/BurgerIcon";
 
 function Navbar() {
   const [openDropdown, setOpenDropdown] = useState<number | null>(null);
@@ -30,19 +32,8 @@ function Navbar() {
               className="text-gray-400 hover:text-gray-900 focus:ring-2 focus:ring-blue-300 focus:outline-none"
               aria-expanded={isMobileMenuOpen}
             >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="h-6 w-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+              <span className="sr-only">Abrir menú principal</span>
+              <BurgerIcon className="h-6 w-6" />
             </button>
           </div>
           <div
@@ -59,18 +50,7 @@ function Navbar() {
                         className="flex w-full items-center justify-between border-b border-gray-100 py-2 pr-4 pl-3 font-medium text-gray-700 hover:bg-gray-50 md:w-auto md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"
                       >
                         {capitalizeFirstLetter(route.path || "home")}
-                        <svg
-                          className="ml-1 h-4 w-4"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                            clipRule="evenodd"
-                          ></path>
-                        </svg>
+                        <DropdownIcon className="ml-1 h-4 w-4" />
                       </button>
                       {/* Dropdown */}
                       <div
