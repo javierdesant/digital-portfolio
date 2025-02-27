0e22c79d4453e52152ee52362d3b8b07b8eaa926
JavierDeSant <j.desantiagosoto@gmail.com>
2025-02-27 20:30:25 +0100

    feat: remake Navbar menu to use MenuItems.
    Replaced the dynamic route-based logic in `Menu` with a simplified implementation using `MenuItems`. Removed `MobileMenuToggle` as it is no longer required. These changes streamline the Navbar code for better readability and maintainability.

--- app/components/Navbar/Menu.tsx
+++ app/components/Navbar/Menu.tsx
@@ -1,39 +1,12 @@
 import React from "react";
-import Dropdown from "./Dropdown";
-import { capitalizeFirstLetter } from "~/utils";
-import type { RouteConfigEntry } from "@react-router/dev/routes";
+import MenuItems from "~/components/Navbar/MenuItems";
 
-interface MenuProps {
-  routes: (RouteConfigEntry | any)[];
-  openDropdown: number | null;
-  toggleDropdown: (key: number) => void;
-}
-
-const Menu: React.FC<MenuProps> = ({
-  routes,
-  openDropdown,
-  toggleDropdown,
-}) => (
-  <ul className="mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium">
-    {routes.map((route, index) => (
-      <li key={index} className="relative">
-        {route.children?.length ? (
-          <Dropdown
-            isOpen={openDropdown === index}
-            toggleDropdown={() => toggleDropdown(index)}
-            route={route}
-          />
-        ) : (
-          <a
-            href={route.path || "/"}
-            className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"
-          >
-            {capitalizeFirstLetter(route.path || "home")}
-          </a>
-        )}
-      </li>
-    ))}
-  </ul>
-);
+const Menu: React.FC = () => {
+  return (
+    <ul className="menu menu-horizontal px-1">
+      <MenuItems />
+    </ul>
+  );
+};
 
 export default Menu;
